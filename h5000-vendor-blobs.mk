9b1ca85c27b741ce4471aa50e2c048288e7401a6
DerTeufel <dominik-kassel@gmx.de>
2017-01-06 17:17:52 +0100

    init

---
+++ h5000-vendor-blobs.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/vendor,system/vendor)
---
+++ h5000-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/uhans/h5000/h5000-vendor-blobs.mk)
