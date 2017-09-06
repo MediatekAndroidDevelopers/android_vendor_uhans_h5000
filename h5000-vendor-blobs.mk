781a2914a17e11552899cff19b97390aba87cb26
fire855 <thefire855@gmail.com>
2017-09-06 22:26:42 +0200

    Move ramdisk services to vendor

--- h5000-vendor-blobs.mk
+++ h5000-vendor-blobs.mk
@@ -8,6 +8,4 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/etc,sys
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/xbin,system/xbin)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/vendor,system/vendor)
