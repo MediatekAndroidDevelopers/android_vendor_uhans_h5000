93335fcbfe74c97e4572a8b381eb004a2ecae493
fire855 <thefire855@gmail.com>
2017-09-06 23:54:00 +0200

    Move everything to vendor

--- h5000-vendor-blobs.mk
+++ h5000-vendor-blobs.mk
@@ -1,11 +1,3 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/lib64,system/lib64)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/h5000/vendor,system/vendor)
